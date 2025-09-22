8cd420204ce7501a1cbc3583e3333c154c157e55
Mahimai Raja J <mahimairaja3@gmail.com>
2025-09-22 10:21:05 -0400

    feat: added helmet middleware

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@
 
 import express from 'express';
 import logger  from '#config/logger.js';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
   logger.info('Hello from API');
